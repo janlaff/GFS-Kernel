c573fa951849970e1d17a14e6fbd45158fc3bec4
proman0973 <jan@lafferton.de>
2017-10-10 20:58:34 +0200

    Query low memory size

--- bootloader/stage2_boot.asm
+++ bootloader/stage2_boot.asm
@@ -6,7 +6,21 @@ jmp stage_two
 %include "bios_print.inc"
 
 stage_two:
+	; Detect low memory
+	clc ; Clear carry flag
+	int 0x12 ; Query low memory size
+	jc low_mem_error ; Exit on error
+
+done:
 	jmp $
+	
+low_mem_error:
+	mov si, low_mem_error_str
+	call bios_print
+	jmp done
+
+low_mem_error_str:
+	db "Failed to get low memory", 13, 10, 0
 
 magic:
 	times (512 * STAGE_TWO_SECTOR_COUNT)-($-$$) db 0
